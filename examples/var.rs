b3dcea927f896e5354b546a3cf239970b49d9a56
hxuvsck <hxuvsck@gmail.com>
2026-01-08 20:32:42 +0800

    Variable lesson on Rust lesson4

---
+++ examples/var.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+
+fn main() {
+    // Variables
+    //  - Immutable by default
+    //  - Use 'mut' keyword to make it mutable
+    let mut x = 1;
+    x += 1;
+    // Type inference
+    // "let x: i32" == "let x"
+    // Expressing the type of variable explicitly is adding "let x: i32" in it.
+}
