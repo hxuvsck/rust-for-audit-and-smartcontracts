3611ed93b7a56a6760dfefdeef106ca8d342517e
hxuvsck <hxuvsck@gmail.com>
2026-01-08 19:59:42 +0800

    Initialized Rust programming language beginner to intermidiate for using audits and smart contracts development

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// cargo init PROJECT_NAME
+// cargo build
+// cargo fmt
+// cargo test
+
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+}
